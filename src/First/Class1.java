45dc16dcece91e58a7faec6a774d6f4bdbfd7e57
Akhila <makhila202@gmail.com>
2018-01-02 13:41:20 -0500

    switch case

--- src/First/Class1.java
+++ src/First/Class1.java
@@ -9,12 +9,22 @@ public class Class1 {
 	 */
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner (System.in);
-		System.out.println("enter 2 values");
-		int first_value = scn.nextInt ();
-		int second_value = scn.nextInt();
-		int c = first_value + second_value;
-		System.out.println("added value is :" + c); 
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter a string");
+		String s1= s.nextLine();
+		//String name="akhila";
+		switch (s1) {
+		case "akhila":
+			System.out.println("its my name");
+			break;
+		case "hari":
+			System.out.println("its my husbnads name");
+			break;
+		default:
+			System.out.println("its my babys name");
+			
+		}
+		
 		
 	}
 
