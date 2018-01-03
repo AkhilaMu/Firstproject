2dd5be90e2e2d5404b33800dedccd010c88dc2ff
Akhila <makhila202@gmail.com>
2018-01-03 17:15:20 -0500

    getters and setters

--- src/First/Class1.java
+++ src/First/Class1.java
@@ -7,18 +7,13 @@ public class Class1 {
 	 */
 
 	public static void main(String[] args) {
-	name ("Hari", 30);
-	int sum =add (10,20,30);
-	int result = sum/2;
-	System.out.println("the result is :"  + result);
-	}
-	public static void name(String str, int age ) {
-	System.out.println("hello my name is " + str + " and my age is " + age);	
-	}
-	
-	public static int add (int a, int b,int c) {
-		return (a+b+c);
-	}
+	Student hari = new Student ();
+	hari.setAge(30);
+	hari.setId(5);
+	hari.setName("Harikrishna Ponakala");
+	hari.setStatus("yes");
+	System.out.println(hari.getName() + " is" + hari.getAge() + " years old" + " id number is " + hari.getId());
+}
 }
 		
 		
