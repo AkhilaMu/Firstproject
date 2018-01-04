548246bfa1527c38ac230c923cea4b845fb34a8f
Akhila <makhila202@gmail.com>
2018-01-03 23:36:41 -0500

    constructor funcationality

--- src/First/Class1.java
+++ src/First/Class1.java
@@ -1,20 +1,20 @@
 package First;
 
 public class Class1 {
-	
-	/*
-	 * my first program
-	 */
-
 	public static void main(String[] args) {
-	Student hari = new Student ();
+	/*Student hari = new Student ();
 	hari.setAge(30);
 	hari.setId(5);
 	hari.setName("Harikrishna Ponakala");
 	hari.setStatus("yes");
 	System.out.println(hari.getName() + " is" + hari.getAge() + " years old" + " id number is " + hari.getId());
-}
-}
+*/
+	Cube c1= new Cube(); 
+	System.out.println(c1.cubedimensions());
+	
+	Cube c2= new Cube(2,2,2);
+	System.out.println(c2.cubedimensions());
+}}
 		
 		
 		
---
+++ src/First/Cube.java
@@ -0,0 +1,26 @@
+package First;
+
+public class Cube {
+	int lenght;
+	int width;
+	int height;
+	Student s;
+	
+	public int cubedimensions() {
+		return (lenght*height*width);
+	}
+	Cube (){
+		lenght = 10;
+		width =10;
+		height= 10;
+		//System.out.println(lenght*width*height);
+	}
+	
+	Cube(int i, int b, int h){
+		
+		
+	}
+	
+	
+	
+}
