38be114120d8c9bf92af5a419a623efa4be21024
Akhila <makhila202@gmail.com>
2018-01-04 12:39:26 -0500

    static funcationality

--- src/First/Class1.java
+++ src/First/Class1.java
@@ -2,24 +2,13 @@ package First;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println(add(1,4));
-		System.out.println(add ("hello", " world"));
-	}
-	/*Student hari = new Student ();
-	hari.setAge(30);
-	hari.setId(5);
-	hari.setName("Harikrishna Ponakala");
-	hari.setStatus("yes");
-	System.out.println(hari.getName() + " is" + hari.getAge() + " years old" + " id number is " + hari.getId());
-*/
-	public static int add (int a, int b) {
-		return(a+b);
-	}
-	public static String add (String a, String b) {
-		return (a+b);
-	}
+	Hello h= new Hello ();
+	Hello h1= new Hello ();
+	Hello.add(5,6);
+	h.sub(6, 5);
+	h1.x=7;
 	
-	
+	}	
 }
 		
 		
