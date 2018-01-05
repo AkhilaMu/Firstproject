67cc29d27fc994cbd0c22feec045cb0e0c0151c7
Akhila <makhila202@gmail.com>
2018-01-05 14:16:55 -0500

    List

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,28 +1,23 @@
 package Project;
-import java.util.ArrayList;;
+import java.util.LinkedList;;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int simple_array[] = new int [5];
-		ArrayList <Integer> list = new ArrayList <Integer> (5);
+		
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(1);
 		list.add(2);
-		list.add(90);
-		list.add(87);
-		//list.add(110);
-		list.add(110);
-		for (Integer a :list) 
-			System.out.println(a);	
-			
-			System.out.println("size is " + list.size());
-			//list.trimToSize();
-			//System.out.println("size is " + list.trimToSize());
-		System.out.println("the removed index value is " +list.remove(2));
-		for (Integer a :list) 
-			System.out.println(a);
+		list.add(3);
+		list.addFirst(4);
+		list.removeLast();
+		//list.clear();
+for (int i :list)
+	System.out.println(i);
+
 		
 		
-		
-		
-	} }
+	} 
+	
+}
 
