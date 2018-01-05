fa1033a82c588f0d8b8ac10e046ab54491add7ba
Akhila <makhila202@gmail.com>
2018-01-05 14:00:23 -0500

    arraylist

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,18 +1,28 @@
 package Project;
+import java.util.ArrayList;;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int simple_array[] = new int [5];
+		ArrayList <Integer> list = new ArrayList <Integer> (5);
+		list.add(2);
+		list.add(90);
+		list.add(87);
+		//list.add(110);
+		list.add(110);
+		for (Integer a :list) 
+			System.out.println(a);	
+			
+			System.out.println("size is " + list.size());
+			//list.trimToSize();
+			//System.out.println("size is " + list.trimToSize());
+		System.out.println("the removed index value is " +list.remove(2));
+		for (Integer a :list) 
+			System.out.println(a);
 		
-		System.out.println(factorial (5));
+		
+		
+		
+	} }
 
-	}
-	
-		public static int factorial (int N) {
-		if (N<=1)
-			return 1;
-		else return (N* factorial (N-1));
-	
-
-}
-}
